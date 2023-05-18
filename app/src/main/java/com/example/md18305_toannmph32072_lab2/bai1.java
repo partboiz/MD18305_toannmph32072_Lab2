6898f7e89fbaea77adb4333740b273d9f9ed40c4
partboiz <mtoan566@gmail.com>
2023-05-19 01:02:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/md18305_toannmph32072_lab2/bai1.java
@@ -0,0 +1,14 @@
+package com.example.md18305_toannmph32072_lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bai1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai1);
+    }
+}
